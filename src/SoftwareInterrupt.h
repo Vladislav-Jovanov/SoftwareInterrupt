0b0534f043b3d542ea50bf81011928cf5e41f4ec
Vladislav Jovanov <vjovanov@nanosys.ihtm.bg.ac.rs>
2024-10-23 07:53:09 +0200

    spaces removed

--- src/SoftwareInterrupt.h
+++ src/SoftwareInterrupt.h
@@ -9,15 +9,15 @@ class Software_Interrupt
         Software_Interrupt(int Pin);
         virtual ~Software_Interrupt();
         void setup(int mode);
-        void set_setup(bool state){setup_finished=state;};        
+        void set_setup(bool state){setup_finished=state;};
         void attachInterrupt(void (*interrupt_func)()=NULL,int Status=CHANGE, int delay_value=NULL);
         void detachInterrupt();
-        void set_init(bool state){init=state;};        
+        void set_init(bool state){init=state;};
         void main();
         bool operator!=(Software_Interrupt const &rhs) const;
     protected:
-        
-        
+
+
     private:
         bool time_passed;
         unsigned long current_time;
@@ -29,7 +29,7 @@ class Software_Interrupt
         bool setup_finished=false;
         bool attach_finished=false;
         bool PIN_state;
-        bool PIN_old_state;        
+        bool PIN_old_state;
         int PIN;
         //static void pass(){};
 
